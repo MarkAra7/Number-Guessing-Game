b5577900a69a52bff97915fc6cd20fa7b211832d
User <nikolajevs04@gmail.com>
2025-11-14 08:49:18 +0200

    little JS

---
+++ app.js
@@ -0,0 +1,17 @@
+console.log("JS works")
+const theNumber =   Math.floor(Math.random() * 100) + 1 ;
+
+const inputBlock = document.getElementById('input')
+const outputBlock = document.getElementById('output')
+const buttonGuesss= document.getElementById('guessButton')
+
+
+buttonGuesss.addEventListener('click',handleClick)
+let guesses =0
+function handleClick(){
+
+
+}
+
+
+console.log(theNumber)
